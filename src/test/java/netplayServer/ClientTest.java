c7ca6571f7d5c602c87b2d7352d8b9bccc5ae06d
Alexander Golec <thejfasi@gmail.com>
2016-05-28 00:30:58 -0400

    Made the code compatible with the code style defined at https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml

--- src/test/java/netplayServer/ClientTest.java
+++ src/test/java/netplayServer/ClientTest.java
@@ -9,28 +9,27 @@ import org.junit.runners.JUnit4;
 
 import netplayprotos.NetplayServiceProto.Port;
 
-@RunWith
-(JUnit4.class)
+@RunWith(JUnit4.class)
 public class ClientTest {
-	
-	private Client client;
-	private Console console;
-	
-	@Before
-	public void setUp() {
-		console = new Console();
-		client = new Client(console, 0);
-		
-	}
-	
-	@Test
-	public void testAddPlayer() {
-		Player player = new Player(Port.PORT_1);
-		assertEquals(0, client.getPorts().size());
-		client.addPlayer(player);
-		assertEquals(1, client.getPorts().size());
-		client.addPlayer(player);
-		assertEquals(1, client.getPorts().size());
-	}
+
+  private Client client;
+  private Console console;
+
+  @Before
+  public void setUp() {
+    console = new Console();
+    client = new Client(console, 0);
+
+  }
+
+  @Test
+  public void testAddPlayer() {
+    Player player = new Player(Port.PORT_1);
+    assertEquals(0, client.getPorts().size());
+    client.addPlayer(player);
+    assertEquals(1, client.getPorts().size());
+    client.addPlayer(player);
+    assertEquals(1, client.getPorts().size());
+  }
 
 }
